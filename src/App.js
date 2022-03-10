66134d72a74ce9563bcdb66c3bdc36ac3668b24a
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-10 17:29:11 +0600

    header part end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Components/CommonPart/Header/Header";
 import Footer from "./Components/CommonPart/Footer/Footer";
 import Home from "./Components/Home/Home";
--- src/Components/CommonPart/Header/Header.js
+++ src/Components/CommonPart/Header/Header.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
-
 const Header = () => {
   return (
-    <div>
-      <h1>This Is Header.</h1>
+    <div className="nav-part">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={NavLink} to="/home" className="nav-logo">
+            Creative Academy
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="ms-auto my-2 my-lg-0 nav-menu"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <NavLink
+                to="/home"
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "black" : "gray",
+                    textDecoration: isActive ? "underline" : "none",
+                  };
+                }}
+              >
+                Home
+              </NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/services">Services</NavLink>
+              <NavLink to="/faq">FAQ</NavLink>
+              <NavLink to="/contact">Contact</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
